ffaac92007088aea60b3b6786716c5975cca934a
turkey <turkey@pplad.com>
2018-03-11 21:21:14 +0000

    Set up linting

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,4 +1,4 @@
 //var chalk = require('chalk');
 import chalk from 'chalk';
 
-console.log(chalk.green('Starting app in the dev mode...'));
+console.log(chalk.green('Starting app in the dev mode...')); //eslint-disable-line no-console
