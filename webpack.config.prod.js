696bf42ea81f29b8d1a0f4528f937df017f4bd05
turkey <turkey@pplad.com>
2018-03-12 18:54:49 +0000

    Finish production module

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,6 +1,8 @@
 import webpack from 'webpack';
 import path from 'path';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import WebpackMd5Hash from 'webpack-md5-hash';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
 
 export default {
   debug: true,
@@ -14,11 +16,17 @@ export default {
   output: {
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/',
-    filename: '[name].js'
+    filename: '[name].[chunkhash].js'
   },
   plugins: [
+    //Generate an external css file with a hash in the filename
+    new ExtractTextPlugin('[name].[contenthash].css'),
 
-    //
+    //Hash the files using MD5 so that their names change when the content changes.
+    new WebpackMd5Hash(),
+
+    //Use CommonChunkPlugin to create a seprate bundle
+    //of vendor libraries so that they're cached separatly
     new webpack.optimize.CommonsChunkPlugin({
       name: "vendor"
     }),
@@ -49,7 +57,8 @@ export default {
   module: {
     loaders: [
       {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
-      {test: /\.css$/, loaders: ['style','css']}
+//      {test: /\.css$/, loaders: ['style','css']}
+      {test: /\.css$/, loader: ExtractTextPlugin.extract('css?sourceMap')}
     ]
   }
 };
