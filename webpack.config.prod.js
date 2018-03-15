e0893f31453325a9e5420e94acbf508a3fbfdfe6
turkey <turkey@pplad.com>
2018-03-15 22:42:36 +0000

    Minor fix for prod config. Delete md5 hash plugin

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,35 +1,24 @@
-import webpack from 'webpack';
 import path from 'path';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
-import WebpackMd5Hash from 'webpack-md5-hash';
 import ExtractTextPlugin from 'extract-text-webpack-plugin';
 
 export default {
-  debug: true,
+  mode: 'production',
   devtool: 'source-map',
-  noInfo: false,
   entry: {
    main: path.resolve(__dirname, 'src/index'),
-   vendor: path.resolve(__dirname, 'vender/index')
+   vendor: path.resolve(__dirname, 'src/vender')
   },
   target: 'web',
   output: {
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/',
-    filename: '[name].[chunkhash].js'
+    filename: '[name].[hash].js'
   },
   plugins: [
     //Generate an external css file with a hash in the filename
     new ExtractTextPlugin('[name].[contenthash].css'),
 
-    //Hash the files using MD5 so that their names change when the content changes.
-    new WebpackMd5Hash(),
-
-    //Use CommonChunkPlugin to create a seprate bundle
-    //of vendor libraries so that they're cached separatly
-    new webpack.optimize.CommonsChunkPlugin({
-      name: "vendor"
-    }),
     // Create HTML file that includes reference to bundled JS.
     new HtmlWebpackPlugin({
       template: 'src/index.html',
@@ -46,19 +35,24 @@ export default {
         minifyURLs: true
       },
       inject: true
-    }),
-
-    // Eliminate duplicate packages when generating bundle
-    new webpack.optimize.DedupePlugin(),
-
-    // Minify JS
-    new webpack.optimize.UglifyJsPlugin()
+    })
   ],
   module: {
-    loaders: [
-      {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
-//      {test: /\.css$/, loaders: ['style','css']}
-      {test: /\.css$/, loader: ExtractTextPlugin.extract('css?sourceMap')}
+    rules: [
+      {test: /\.js$/, exclude: /node_modules/, loaders: ['babel-loader']},
+      {test: /\.css$/, loaders: ['style-loader','css-loader']}
     ]
+  },
+  optimization: {
+      minimize: true,
+      splitChunks: {
+        chunks: 'all'
+      }
+  },
+  resolve:
+  {
+    alias: {
+      'handlebars' : 'handlebars/dist/handlebars.js'
+    }
   }
 };
